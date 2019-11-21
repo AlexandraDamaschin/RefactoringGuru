b7081831b49353cb5e127419c51c3c7d4ec375dd
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2019-11-21 21:55:57 +0200

    Factory method implemented

---
+++ BuilderPattern/FactoryMethod/Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    class Client
+    {
+        public void Main()
+        {
+            Console.WriteLine("App: Launched with the ChildOfCreator1.");
+            ClientCode(new ChildOfCreator1());
+
+            Console.WriteLine("");
+
+            Console.WriteLine("App: Launched with the ChildOfCreator2.");
+            ClientCode(new ChildOfCreator2());
+        }
+
+        public void ClientCode(Creator creator)
+        {
+            Console.WriteLine("Client: I'm not aware of the creator's class," +
+                "but it still works.\n" + creator.Operation());
+        }
+    }
+}
