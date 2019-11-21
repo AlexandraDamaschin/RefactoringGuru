da367e1a1cf10f937547a7834c631516e80778fb
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2019-11-21 21:51:30 +0200

    Implement interface and factory method

---
+++ BuilderPattern/FactoryMethod/Creator.cs
@@ -0,0 +1,33 @@
+﻿namespace FactoryMethod
+{
+    abstract class Creator
+    {
+        public abstract IProduct FactoryMethod();
+
+        public string Operation()
+        {
+            var product = FactoryMethod();
+
+            var result = "Creator worked with:" + product.Operation();
+
+            return result;
+        }
+    }
+
+    // other creators which inherit the Creator
+    class ChildOfCreator1 : Creator
+    {
+        public override IProduct FactoryMethod()
+        {
+            return new Product1();
+        }
+    }
+
+    class ChildOfCreator2 : Creator
+    {
+        public override IProduct FactoryMethod()
+        {
+            return new Product2();
+        }
+    }
+}
---
+++ BuilderPattern/FactoryMethod/IProduct.cs
@@ -0,0 +1,24 @@
+﻿namespace FactoryMethod
+{
+    public interface IProduct
+    {
+        string Operation();
+    }
+
+    //other classes which implements the product interface
+    class Product1 : IProduct
+    {
+        public string Operation()
+        {
+            return "{Result of product 1}";
+        }
+    }
+
+    class Product2 : IProduct
+    {
+        public string Operation()
+        {
+            return "{Result of product 2}";
+        }
+    }
+}
