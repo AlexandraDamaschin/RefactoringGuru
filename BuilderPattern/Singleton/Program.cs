5ec57096c5d86281a5c495891e0c87babcbc7196
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2019-12-01 13:04:58 +0200

    Easy implementation of singleton

--- BuilderPattern/Singleton/Program.cs
+++ BuilderPattern/Singleton/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace Singleton
+﻿using System;
+
+namespace Singleton
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton works, both variables contain the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed, variables contain different instances.");
+            }
         }
     }
 }
---
+++ BuilderPattern/Singleton/Singleton.cs
@@ -0,0 +1,23 @@
+﻿namespace Singleton
+{
+    class Singleton
+    {
+        //private to prevent direct construction calls with the `new` operator.
+        private Singleton() { }
+
+        private static Singleton _instance;
+
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+
+        public static void someBusinessLogic()
+        {
+        }
+    }
+}
